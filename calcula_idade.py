ba5905b3560037b1d834deb49a7e3e0e5e8827af
Gabriel-Rehbein <gabrielmr0812@gmail.com>
2025-03-27 20:29:02 -0300

    atualização

--- calcula_idade.py
+++ calcula_idade.py
@@ -1,9 +1,15 @@
 from datetime import datetime
 
-data_nasc = input("Digite sua data de nsciemnto (dd/mm/yyyy):")
+data_nasc = input("Digite sua data de nascimento (dd/mm/yyyy): ")
 nascimento = datetime.strptime(data_nasc, "%d/%m/%Y")
 
 hoje = datetime.today()
 idade = hoje.year - nascimento.year
-idade = idade.year -nascimento.year
+
+if (hoje.month, hoje.day) < (nascimento.month, nascimento.day):
+    idade -= 1
+
 print(f"Você tem {idade} anos.")
+
+if idade >= 18:
+    print("Você pode tirar a CNH.")
